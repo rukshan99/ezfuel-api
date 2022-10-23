369f0f5f52ab79c301a7a2e83b1c4462831f9686
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-10-23 21:07:12 +0530

    getAllShedsByLocation endpoint

--- routes/shed.routes.js
+++ routes/shed.routes.js
@@ -8,6 +8,7 @@ const ShedController = require('../controllers/shed.controller');
 */
 router.post('/sheds', ShedController.createShed);
 router.get('/sheds', ShedController.getAllSheds);
+router.get('/sheds/:district/:city', ShedController.getAllShedsByLocation);
 router.get('/sheds/:shedId', ShedController.getShedById);
 router.put( '/sheds/sales', ShedController.recordSale);
 
